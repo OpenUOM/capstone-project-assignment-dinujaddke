c63d74280f711847ed01ce9ba67a55f4069cfe37
Dinuja Elesinghe <dinujaddk@gmail.com>
2025-06-18 04:18:12 +0530

    database function changes 7

--- frontend/test/student-add.test.js
+++ frontend/test/student-add.test.js
@@ -6,6 +6,7 @@ fixture`Testing Student UI`
 
 test('Testing add students', async t => {
     await t.navigateTo("/dbinitialize");
+    await t.wait(3000);  // wait for DB init
 
     await t.navigateTo("/addStudent");
     await t.typeText("#student-id", "999999");
@@ -14,12 +15,13 @@ test('Testing add students', async t => {
     await t.typeText("#student-Hometown", "Catholic");
     await t.click("#student-add");
 
-    await t.wait(1000); // wait for add to complete
+    await t.wait(3000);  // wait for add to complete
 
     await t.navigateTo("/student");
 
     const table = Selector('#student-table');
-    const studentRow = table.find('tr').withText('Pasindu Basnayaka');
+    await t.expect(table.exists).ok({ timeout: 5000 });  // wait for table
 
+    const studentRow = table.find('tr').withText('Pasindu Basnayaka');
     await t.expect(studentRow.exists).ok({ timeout: 5000 });
 });
